d4d127ae3d8d0b91a7524f904c0ebd605fe9a9af
bass-2000 <bass-2000@yandex.ru>
2018-07-04 11:53:23 +0300

    tasks

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2001/Solution.java
@@ -0,0 +1,102 @@
+package com.javarush.task.task20.task2001;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 
+Читаем и пишем в файл: Human
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+        try {
+            File your_file_name = File.createTempFile("your_file_name", null);
+            OutputStream outputStream = new FileOutputStream(your_file_name);
+            InputStream inputStream = new FileInputStream(your_file_name);
+
+            Human ivanov = new Human("Ivanov", new Asset("home", 999_999.99), new Asset("car", 2999.99));
+            ivanov.save(outputStream);
+            outputStream.flush();
+
+            Human somePerson = new Human();
+            somePerson.load(inputStream);
+            inputStream.close();
+            //check here that ivanov equals to somePerson - проверьте тут, что ivanov и somePerson равны
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with my file");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with save/load method");
+        }
+    }
+
+    public static class Human {
+        public String name;
+        public List<Asset> assets = new ArrayList<>();
+
+        public Human() {
+        }
+
+        public Human(String name, Asset... assets) {
+            this.name = name;
+            if (assets != null) {
+                this.assets.addAll(Arrays.asList(assets));
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Human human = (Human) o;
+
+            if (name != null ? !name.equals(human.name) : human.name != null) return false;
+            return assets != null ? assets.equals(human.assets) : human.assets == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + (assets != null ? assets.hashCode() : 0);
+            return result;
+        }
+
+        public void save(OutputStream outputStream) throws Exception {
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.println(name);//первая строка сейва
+            if (!assets.isEmpty()) {
+                printWriter.println("yes"); //вторая строка - наличие активов
+                for (Asset asset : assets) {
+                    printWriter.println(asset.getName()); //третья строка
+                    printWriter.println(String.valueOf(asset.getPrice())); //четвертая строка
+                }
+            }
+            else printWriter.println("no");
+            printWriter.close();
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            this.name = reader.readLine();//1 строка
+            String areThereAssets = reader.readLine();//2 строка
+            if (areThereAssets.equals("yes")) {
+                String line;
+                StringBuilder sb = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append(System.getProperty("line.separator"));//считываю построчно с разделителями строк
+                }
+                String[] splitted = sb.toString().split(System.getProperty("line.separator"));
+
+                for (int i = 0; i < splitted.length; i = i + 2){ //0, 2, 4
+                    assets.add(new Asset(splitted[i], Double.parseDouble(splitted[i+1])));
+                }
+            }
+            reader.close();
+        }
+    }
+}
